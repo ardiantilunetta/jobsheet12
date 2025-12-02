884994c9e249954e6bffeff76106fb8ebcf0a1fd
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-12-02 22:57:43 +0700

    Tugas 3b modifikasi

--- RekapPenjualanCafe18.java
+++ RekapPenjualanCafe18.java
@@ -3,29 +3,42 @@ import java.util.Scanner;
 public class RekapPenjualanCafe18 {
 
     public static void main(String[] args) {
-        String[] menu = {"Kopi\t", "Teh\t", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
-        int[][] rekap = new int[5][7];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah menu : ");
+        int jumlahMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah hari : ");
+        int jumlahHari = sc.nextInt();
+        String[] menu = new String[jumlahMenu];
+        int[][] rekap = new int[jumlahMenu][jumlahHari];
         isiArray(rekap, menu);
-        tampilArray(rekap, menu);
+        tampilArray(rekap, menu, jumlahHari);
         tertinggi(rekap, menu);
         rata(rekap, menu);
     }
 
-    public static void isiArray(int rekap[][], String menu[]) {
+    public static void isiArray(int rekap[][], String[] menu) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0 ; i < rekap.length ; i++) {
-            System.out.println("Masukkan Penjualan "+menu[i]);
+            System.out.println();
+            System.out.print("Menu "+(i+1)+" : ");
+            menu[i] = sc.nextLine();
             for (int j = 0 ; j < rekap[i].length ; j++) {
                 System.out.print("Hari ke-"+(j+1)+" : ");
                 rekap[i][j] = sc.nextInt();
             }
+            sc.nextLine();
         }
         System.out.println();
     }
 
-    public static void tampilArray (int rekap[][], String menu[]) {
+    public static void tampilArray (int rekap[][], String menu[], int hari) {
         System.out.println("----- REKAP PENJUALAN -----");
-        System.out.println("Menu\t\tH1\tH2\tH3\tH4\tH5\tH6\tH7");
+        System.out.print("Menu\t\t");
+        for (int i = 0 ; i < hari ; i++) {
+            System.out.print("H"+(i+1)+"\t");
+        }
+        System.out.println();
+        // System.out.println("Menu\t\tH1\tH2\tH3\tH4\tH5\tH6\tH7");
         for (int i = 0 ; i < rekap.length ; i++) {
             System.out.print(menu[i]+"\t");
             for (int j = 0 ; j < rekap[i].length ; j++) {
@@ -50,7 +63,7 @@ public class RekapPenjualanCafe18 {
             }
         }
         System.out.println("----- PENJUALAN TERTINGGI -----");
-        System.out.println(nama+penjualan);
+        System.out.println(nama+"\t"+penjualan);
         System.out.println();
     }
 
