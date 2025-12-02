18d33a4d2e65d192f48f6a4b373c2d72a6ac2794
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-12-02 21:14:59 +0700

    Tugas 2 fungsi isianArray

---
+++ NilaiMahasiswa18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NilaiMahasiswa18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int n = sc.nextInt();
+        int[] nilai = new int[n];
+        isianArray(n, nilai);
+    }
+
+    public static void isianArray (int jumlah, int[] nilai) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0 ; i < jumlah ; i++) {
+            System.out.print("Masukkan nilai mahasiswa "+(i+1)+" : ");
+            nilai[i] = sc.nextInt();
+        }
+    }
+}
