7f3d9a7d23fc73f6afb987545aa14ad85f7ad4f3
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-12-02 21:05:09 +0700

    tugas 1 fungsi luas permukaan

--- Kubus18.java
+++ Kubus18.java
@@ -6,10 +6,16 @@ public class Kubus18 {
         System.out.print("Masukkan panjang sisi : ");
         int sisi = sc.nextInt();
         System.out.println("Volume kubus = "+volume(sisi));
+        System.out.println("Luas permukaan = "+luasPermukaan(sisi));
     }
 
     public static int volume(int s) {
         int volume = s*s*s;
         return volume;
+    }
+
+    public static int luasPermukaan(int s) {
+        int lp = 6*s*s;
+        return lp;
     }
 }
